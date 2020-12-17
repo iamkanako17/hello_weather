540a806740d9f1bce72e706b48fdc5a9648c4652
iamkanako17 <kanako.kk8787tw@gmail.com>
2020-12-17 21:33:05 +0900

    Geolocationで現在地取得

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-import pip._vendor.requests
-import json
-import urllib.request
-import time
-import pip._vendor.requests.api
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, requests, json, os
+# from jinja2 import Template, Environment, FileSystemLoader
 
-api_key = {"8cac92499bde2ad6dcf5ec1e3a1c9691"}
-# api_url = "api.openweathermap.org/data/2.5/weather?lat={a}&lon={b}&appid={c}" .format(a=lat, b=lng, c=apikey)
+app = Flask(__name__)
+geo_key = "AIzaSyDyR6n90hQQiz88u3JeWO8dwC4Cbjzmnb8"
+weather_key = "8cac92499bde2ad6dcf5ec1e3a1c9691"
+weather_api= "http://api.openweathermap.org/data/2.5/weather?lat={a}&lon={b}&appid={c}" .format(a=lat, b=lng, c=weather_key)
 
 
 @app.route('/')
-def top_page():
-    return render_template("index.html")
+def index():
+    render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
--- static/geo.js
+++ static/geo.js
@@ -1,8 +1,14 @@
-if( navigator.geolocation ) {
-    alert("現在地取得成功");
-    navigator.geolocation.getCurrentPosition((position) => {
-      doSomething(position.coords.latitude, position.coords.longitude);
-    });
-} else {
-    alert("現在地取得未対応");
+
+function success(pos) {
+    var lat = pos.coords.latitude;
+    var lng = pos.coords.longitude;
+
+    document.getElementById("location").innerHTML = location;
+    weatherLink = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=8cac92499bde2ad6dcf5ec1e3a1c9691`;
 }
+
+function fail(error) {
+  window.alert('位置情報の取得に失敗しました。 エラーコード:' + error.code);
+}
+navigator.geolocation.getCurrentPosition(success, fail);
+
