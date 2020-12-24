6ee2eda232c476eb4f7c9267d07eca9324a91031
iamkanako17 <kanako.kk8787tw@gmail.com>
2020-12-24 18:55:36 +0900

    Add error handling

--- static/geo.js
+++ static/geo.js
@@ -1,12 +1,22 @@
+const status = document.querySelector('#status');
 
-window.onload = function getPosition() {
-    navigator.geolocation.getCurrentPosition(success);
-    function success(pos) {
-        var lat = pos.coords.latitude;
-        var lon = pos.coords.longitude;
-        
-        document.getElementById("lat").value = lat;
-        document.getElementById("lon").value = lon;
-        document.forms["send"].submit();
-    }
+function success(pos) {
+    var lat = pos.coords.latitude;
+    var lon = pos.coords.longitude;
+    
+    document.getElementById("lat").value = lat;
+    document.getElementById("lon").value = lon;
+    document.forms["send"].submit();
+}
+
+
+function error() {
+    status.textContent = '位置情報を取得できません。設定をお確かめください。';
+}
+
+if(!navigator.geolocation) {
+    status.textContent = 'ご使用のブラウザでは位置情報の取得ができません。';
+} else {
+    status.textContent = 'Getting your position…';
+    navigator.geolocation.getCurrentPosition(success, error);
 }
